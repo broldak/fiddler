a27a96f223bf6c9f34c12a1141b3b9de25db8759
Brian Oldak <boldak@andrew.cmu.edu>
2014-07-12 05:23:00 -0700

    js

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-from google.appengine.api import files
-
 #videos = UploadSet('videos', MOVIES)
 
 @app.route('/user/<username>')
@@ -31,24 +29,25 @@ def uploadVideo(file, title):
 
 
 @app.route('/event/<int:event_id>')
-def show_event(event_id):
+def show_event(event_id, name='Event1'):
   #video = Video.load(event_id)
   #if video is None:
   #  abort(404);
   #url = videos.url(video.filename)
-  return render_template('event.html')
+  return render_template('event.html', name=name)
+
+@app.route('/event/create')
+def create_event():
+  return render_template('create_event.html')
 
 @app.route('/')
 def home():
     """Default Home Page."""
     return render_template('home.html')
 
-@app.route('/test')
-def test():
-    """Test (for testing purposes."""
-    return "Test"
-
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
     return 'Sorry, nothing at this URL.', 404
+
+app.run()
