474ab8841d4b3821460b89e1ac4ac333793d9fd7
Nitesh <nvd5@cornell.edu>
2014-07-11 21:34:30 -0700

    adding simple routes for now

--- main.py
+++ main.py
@@ -6,11 +6,31 @@ app.config['DEBUG'] = True
 # the App Engine WSGI application server.
 
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return render_template('layout.html')
+@app.route('/event/<int:event_id>')
+def show_event(event_id):
+	#event_id will be an integer
+    """Show an event instance."""
+    return render_template('event.html')
 
+@app.route('/user/<username>')
+def show_user(username):
+    """Show a user account instance."""
+    return render_template('user.html')
+
+@app.route('/upload')
+def upload():
+    """Show the page to allow a video upload."""
+    return render_template('upload.html')
+
+@app.route('/home')
+def home():
+    """Default Home Page."""
+    return render_template('home.html')
+
+@app.route('/test')
+def test():
+    """Test (for testing purposes."""
+    return "Test"
 
 @app.errorhandler(404)
 def page_not_found(e):
